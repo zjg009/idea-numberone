35309915ed638143b07829bf41341dafdd1ea669
zjg009 <1364396515@qq.com>
2020-08-22 23:22:36 +0800

    提交2

--- numberone-admin/src/main/java/com/numberone/NumberoneApplication.java
+++ numberone-admin/src/main/java/com/numberone/NumberoneApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-/**
+/***
  * 启动程序
  * 
  * @author numberone
